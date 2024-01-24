8407b84954672210e06275104d8d78cd25272bf4
HexHelix <aklantabhuyan@gmail.com>
2024-01-24 16:59:11 +0530

    Added two player support. Added currentPlayer boolean to keep trach of current player. If player encounters ladder bool value is not changed. Else its changed to give the other player his turn.

--- snakeAndLadder/Program.cs
+++ snakeAndLadder/Program.cs
@@ -11,57 +11,112 @@ namespace snakeAndLadder
         static void Main(string[] args)
         {
             Int32 playerOnePos = 0;
+            Int32 playerTwoPos = 0;
+            bool currentPlayer = true;
             Int32 RoundsPlayed = 0;
             Console.WriteLine("Welcome Player to Snake And Ladder");
             
-            while(playerOnePos != 100)
+            while(playerOnePos != 100 || playerTwoPos != 100)
             {
-                Console.WriteLine("Press enter key to roll dice");
-                //Console.ReadLine();
-                int dice = DiceRoll();
-                
-                Console.WriteLine(dice + " was obtained from dice roll");
-                int playoption = PlayOption();
 
-                if (playoption == 1)
+                if (currentPlayer)
                 {
-                    Console.WriteLine("No Play, player position is " + playerOnePos);
-                }
-                else if(playoption == 2)
-                {
+                    Console.WriteLine("Player 1, Press enter key to roll dice");
+                    Console.ReadLine();
+                    int dice = DiceRoll();
 
-                    //playerOnePos += dice;
-                    if(playerOnePos + dice > 100)
+                    Console.WriteLine(dice + " was obtained from dice roll");
+                    int playoption = PlayOption();
+
+                    if (playoption == 1)
                     {
-                        Console.WriteLine("Need " + (100 - playerOnePos) + "to win.");
-                        continue;
+                        Console.WriteLine("No Play, player position is " + playerOnePos);
+                        currentPlayer = !currentPlayer;
+                    }
+                    else if (playoption == 2)
+                    {
+
+                        //playerOnePos += dice;
+                        if (playerOnePos + dice > 100)
+                        {
+                            Console.WriteLine("Need " + (100 - playerOnePos) + "to win.");
+                            continue;
+                        }
+                        else
+                        {
+                            playerOnePos += dice;
+                            Console.WriteLine("Ladder!! , player moved to position " + playerOnePos);
+                        }
+
+                    }
+                    else if (playoption == 3)
+                    {
+                        playerOnePos -= dice;
+                        if (playerOnePos < 0) playerOnePos = 0;
+                        Console.WriteLine("Oh nooo Snake! , player moved to position " + playerOnePos);
+                        currentPlayer = !currentPlayer;
                     }
                     else
                     {
-                        playerOnePos += dice;
-                        Console.WriteLine("Ladder!! , player moved to position " + playerOnePos);
+                        Console.WriteLine("Error");
                     }
-                    
                 }
-                else if(playoption == 3)
+                else if (!currentPlayer)
                 {
-                    playerOnePos -= dice;
-                    if(playerOnePos < 0)playerOnePos = 0;
-                    Console.WriteLine("Oh nooo Snake! , player moved to position " + playerOnePos);
-                }
-                else
-                {
-                    Console.WriteLine("Error");
+                    Console.WriteLine("Player 2! Press enter key to roll dice");
+                    Console.ReadLine();
+                    int dice = DiceRoll();
+
+                    Console.WriteLine(dice + " was obtained from dice roll");
+                    int playoption = PlayOption();
+
+                    if (playoption == 1)
+                    {
+                        Console.WriteLine("No Play, player position is " + playerTwoPos);
+                        currentPlayer = !currentPlayer;
+                    }
+                    else if (playoption == 2)
+                    {
+
+                        //playerOnePos += dice;
+                        if (playerTwoPos + dice > 100)
+                        {
+                            Console.WriteLine("Need " + (100 - playerTwoPos) + "to win.");
+                            continue;
+                        }
+                        else
+                        {
+                            playerTwoPos += dice;
+                            Console.WriteLine("Ladder!! , player moved to position " + playerTwoPos);
+                        }
+
+                    }
+                    else if (playoption == 3)
+                    {
+                        playerTwoPos -= dice;
+                        if (playerTwoPos < 0) playerTwoPos = 0;
+                        Console.WriteLine("Oh nooo Snake! , player moved to position " + playerTwoPos);
+                        currentPlayer = !currentPlayer;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                    }
                 }
 
                 RoundsPlayed++;
                 
 
 
+            } 
+             if (playerOnePos == 100)
+            {
+                Console.WriteLine("Congrats player 1!!! you reached the finish line!. Number of moves played is " + RoundsPlayed);
             }
-
-            Console.WriteLine("Congrats!!! you reached the finish line!. Number of moves played is " + RoundsPlayed );
-
+             else if(playerTwoPos == 100)
+            {
+                Console.WriteLine("Congrats player 2!!! you reached the finish line!. Number of moves played is " + RoundsPlayed);
+            }
 
         }
 
