5f7e01c3b4761853f346e7d8d24a3fe8b3e31684
HexHelix <aklantabhuyan@gmail.com>
2024-01-24 16:18:19 +0530

    Added logic in ladder to make sure exact dice roll is required to reach to 100.

--- snakeAndLadder/Program.cs
+++ snakeAndLadder/Program.cs
@@ -12,7 +12,44 @@ namespace snakeAndLadder
         {
             Int32 playerOnePos = 0;
             Console.WriteLine("Welcome Player to Snake And Ladder");
-            Console.WriteLine(PlayOption());
+            
+            while(playerOnePos != 100)
+            {
+                Console.WriteLine("Press enter key to roll dice");
+                int dice = DiceRoll();
+                Console.WriteLine(dice + " was obtained from dice roll");
+                int playoption = PlayOption();
+                
+                if(playoption == 1)
+                {
+                    Console.WriteLine("No Play, player position is " + playerOnePos);
+                }
+                else if(playoption == 2)
+                {
+
+                    //playerOnePos += dice;
+                    if(playerOnePos + dice > 100)
+                    {
+                        Console.WriteLine("Need " + (100 - playerOnePos) + "to win.");
+                        continue;
+                    }
+                    else
+                    {
+                        playerOnePos += dice;
+                        Console.WriteLine("Ladder!! , player moved to position " + playerOnePos);
+                    }
+                    
+                }
+                else if(playerOnePos == 3)
+                {
+                    playerOnePos -= dice;
+                    if(playerOnePos < 0)playerOnePos = 0;
+                    Console.WriteLine("Oh nooo Snake! , player moved to position " + playerOnePos);
+                }
+
+            }
+
+            Console.WriteLine("Congrats!!! you reached the finish line!");
 
 
         }
